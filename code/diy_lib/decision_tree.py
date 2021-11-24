f3e2d72995b27481b0bf5ca2396ee5c8b36954f1
NicolaClark <niccl@itu.dk>
2021-11-24 14:21:29 +0000

    node class

---
+++ code/diy_lib/decision_tree.py
@@ -0,0 +1,25 @@
+class Decision_tree:
+    def __init__(self,data,depth):
+        self.depth=depth
+        self.data=data
+    
+    def create_child(self):
+        pass
+    def _split(self):
+        pass
+    def fit(self):
+        pass
+
+
+
+##TODO
+#innit method
+    #make tree at correct depth
+#fit
+    #assign info where split, how much data in each split?
+#
+
+def main():
+    pass
+if __name__ == "__main__":
+    main()
---
+++ code/diy_lib/node.py
@@ -0,0 +1,46 @@
+class Node:
+    def __init__(self,name,parent,level,feature=None,threshold=None,gini_val=None):
+        self.name = name
+        self.parent = parent
+        self.level = level
+        self.gini_val = gini_val #none as defualt
+        self.threshold = threshold #none as defualt
+        self.left_child = None #none as defualt
+        self.right_child = None #none as defualt
+
+    def create_children(self,lchild,rchild):
+        self.left_child = lchild
+        self.right_child = rchild
+        #make a child
+        
+    def parent(self):
+        return self.parent
+    
+    def children(self):
+        return self.left_child,self.right_child
+    
+    def gini_val(self):
+        return self.gini_val
+    
+    def feature(self):
+        return self.feature
+    
+    def threshold(self):
+        return self.threshold
+    
+    def get_level(self):
+        return self.level
+        
+    def get_position(self):
+        if self.parent==None:
+            return("root")
+        elif self.left_child == None:
+            return('leaf')
+        else:
+            return f"node at depth {self.get_level()}"
+
+
+def main():
+    pass
+if __name__=="__main__":
+    main()
